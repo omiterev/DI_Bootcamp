7079e71d2eadbade95b779531bec07da9bc5dae5
omiterev <omitereva@gmail.com>
2025-08-20 13:07:00 +0300

    day1 ex1

---
+++ week1/day1/XP_exercises/exercise1.py
@@ -0,0 +1,42 @@
+#1 exercise
+print("Hello world\n"*4)
+#2 exercise
+x=(99**3)*8
+print(x)
+#3 exercise
+False
+True
+False
+False
+False
+#4 exercise
+computer_brand = "ThinkPad"
+print(f"I have a {computer_brand} computer.")
+#5 exercise
+name = "Olga"
+age = 29
+shoe_size = 39
+print(f"My name is {name}, I'm {age} and my shoe size is {shoe_size}.")
+#6 exercise
+a=11
+b=9
+if a > b:
+    print("Hello World")
+#7 exercise
+u_number=int(input("Enter a number: "))
+if u_number % 2==0:
+    print("even")
+else:
+    print("odd")
+#8 exercise
+u_name = input("Enter your name: ")
+if u_name == "Olga":
+    print("We have the same name")
+else:
+    print("My name is cooler")
+#9 exercise
+height = float(input("Enter your height"))
+if height > 145:
+    print("You are tall enough to ride.")
+else:
+    print("You need to grow some more to ride.")
