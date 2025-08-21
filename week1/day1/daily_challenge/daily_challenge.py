f9feaa80167c0cb6e2c6f8100f666710654c1199
omiterev <omitereva@gmail.com>
2025-08-21 16:08:06 +0300

    exercise redo

--- week1/day1/daily_challenge/daily_challenge.py
+++ week1/day1/daily_challenge/daily_challenge.py
@@ -8,11 +8,11 @@ else:
     print("Perfect string")
     print(u_string[0], u_string[-1])
     new_string =""
-for l in u_string:
-    new_string += l
-    print(new_string)
+    for l in u_string:
+        new_string += l
+        print(new_string)
 
-list_string = list(u_string)
-random.shuffle(list_string)
-shuffle_string = "".join(list_string)
-print(shuffle_string)
+    list_string = list(u_string)
+    random.shuffle(list_string)
+    shuffle_string = "".join(list_string)
+    print(shuffle_string)
